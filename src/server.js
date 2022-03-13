9cea1c3b56d5ceb69edaef681e399edc243e6791
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-03-13 15:40:28 -0300

    server

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import http from 'http';
+
+import {
+    Readable
+} from 'stream'
+
+import {
+    randomUUID
+} from 'crypto'
+
+function* run() {
+    for (let index = 0; index <= 99; index++) {
+        const data = {
+            id: randomUUID(),
+            name: `Fulano-${index}`
+        }
+        yield data
+    }
+}
+
+async function handler(requeste, response) {
+    const readable = Readable({
+        read() {
+            for (const data of run()) {
+                console.log(`sending`, data)
+                this.push(JSON.stringify(data) + "\n")
+            }
+            this.push(null) //info que os dados acabaram
+        }
+    })
+    readable
+        .pipe(response)
+}
+
+http.createServer(handler)
+    .listen(3000)
+    .on('listening', () => console.log('server running at 3000'))
